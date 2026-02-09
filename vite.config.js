80a4d707db116aa4022a33854647b21bc8f50832
Sujoy <sujoy143656@gmail.com>
2026-02-10 01:09:22 +0530

    Remove SCSS and sass dependency

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: "/task-management-system/",
+  plugins: [react(), tailwindcss()],
+  base: "/task-management-system/", // trailing slash here is important
 })
+
